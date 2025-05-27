b6379cf3491aac595eeea9ec10f84121256605e7
pat56-hp <patrickkouassi7@gmail.com>
2025-05-27 16:26:36 +0000

    Updated: Mise a jour de la generation du contrat

--- app/Http/Controllers/Api/Biens/ContratController.php
+++ app/Http/Controllers/Api/Biens/ContratController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api\Biens;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appartement;
+use App\Models\Locataire;
+use App\Models\Proprietaire;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ContratController extends Controller
@@ -15,7 +18,7 @@ class ContratController extends Controller
      * @param Request $request
      * @return void
      */
-    public function __invoke(Request $request)
+    /* public function __invoke(Request $request)
     {
         $request->validate([
             'proprietaire' => 'required|string',
@@ -73,6 +76,65 @@ class ContratController extends Controller
             return response()->json([
                 'message' => $th->getMessage(),
                 'data' => $th->getMessage()
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    } */
+
+    public function generateContrat(Request $request)
+    {
+        $request->validate([
+            'locataire' => 'required|exists:locataires,id',
+            'appartement' => 'required|exists:appartements,id',
+            'proprietaire' => 'required|exists:proprietaires,id',
+            'date_debut' => 'required|date',
+            'date_fin' => 'required|date|after:date_debut',
+            'garantie' => 'required|integer',
+            'type' => 'required|string'
+        ]);
+
+        try {
+            $locataire = Locataire::findOrFail($request->locataire);
+            $appartement = Appartement::findOrFail($request->appartement);
+            $proprietaire = Proprietaire::findOrFail($request->proprietaire);
+            $date_debut = Carbon::parse($request->date_debut);
+            $date_fin = Carbon::parse($request->date_fin);
+            $type = $request->type;
+
+            $html = view('contrats.modele', [
+                'nom_proprietaire' => $proprietaire->name,
+                'contact_proprietaire' => $proprietaire->phone ?? 'N/A',
+                'email_proprietaire' => $proprietaire->email ?? 'N/A',
+                'adresse_proprietaire' => $proprietaire->address ?? 'N/A',
+
+                'nom_locataire' => $locataire->nom_complet,
+                'contact_locataire' => $locataire->telephone ?? 'N/A',
+                'email_locataire' => $locataire->email ?? 'N/A',
+                'adresse_locataire' => $locataire->adresse ?? 'N/A',
+
+                'adresse_appartement' => $appartement->adresse ?? 'N/A',
+                'contrat_type' => $type,
+
+                'duree' => $date_debut->diffInYears($date_fin),
+                'date_debut' => $date_debut->format('d F Y'),
+                'date_fin' => $date_fin->format('d F Y'),
+
+                'garantie' => $request->garantie,
+                'garantie_amount' => number_format($appartement->loyer, 0, '.', ' ') . ' FCFA',
+
+                'loyer' => $appartement->loyer_formatted,
+                'charges_formatted' => $appartement->charges_formatted,
+
+                'contrat_adresse' => $proprietaire->address,
+                'contrat_date' => now()->format('d/m/Y')
+            ])->render();
+
+            return response()->json([
+                'data' => $html,
+                'message' => 'Génération de contrat est un succès'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage()
             ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
